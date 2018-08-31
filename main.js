2b8fff32329d080eb73ce4e995c3a3cfbef2b6e1
siguoya <924714558@qq.com>
2018-08-31 14:56:38 +0800

    完成基本功能

--- main.js
+++ main.js
@@ -1,50 +1,55 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const {ipcMain, app, Menu, BrowserWindow, Tray} = require('electron')
+const path = require('path')
+const axios = require('axios')
 let mainWindow
+let appIcon = null
+//不在dock上显示icon
+app.dock.hide();
+ipcMain.on('put-in-tray', (event) => {
+	//初始化appIcon
+	const iconPath = path.join(__dirname, 'logo.jpg')
+	appIcon = new Tray(iconPath)
+	const contextMenu = Menu.buildFromTemplate([{
+		label: '退出',
+		click: () => {
+			event.sender.send('tray-removed')
+		}
+	}])
+	appIcon.setToolTip('软银投资的黄金特色理财平台')
+	appIcon.setTitle('查询中...')
+	appIcon.setContextMenu(contextMenu)
+	//更新appIcon的title
+	setInterval(() => {
+		axios.post('https://www.g-banker.com/price/query', {
+			"queryFlag": 3
+		}).then((response) => {
+			let price = response['data']['data']['realtime_price']
+			price = (price / 100).toFixed(2).toString();
+			appIcon.setTitle(price)
+		}).catch((error) => {
+			console.log(error)
+		})
+	}, 3000)
+})
+ipcMain.on('remove-tray', () => {
+	appIcon.destroy()
+	app.quit()
+})
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+function ready() {
+	mainWindow = new BrowserWindow({width: 800, height: 600, show: false})
+	mainWindow.loadFile('index.html')
+	mainWindow.on('closed', function () {
+		mainWindow = null
+	})
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
+app.on('ready', ready)
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+	if (appIcon) {
+		appIcon.destroy()
+	}
 })
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
