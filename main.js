294676214bd519a9e403649d0b08b14546572edf
siguoya <924714558@qq.com>
2018-09-01 12:03:33 +0800

    优化非交易时间段的查询

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const {ipcMain, app, Menu, BrowserWindow, Tray} = require('electron')
 const path = require('path')
 const axios = require('axios')
 const moment=require('moment')
-let mainWindow
-let appIcon = null
+let mainWindow=null//浏览器窗口
+let appIcon = null//系统托盘
+let should_query=true;//是否有必要查询，例如非交易日只查询一次就行了
 //不在dock上显示icon
 if (process.platform == 'darwin') {
 	app.dock.hide();
 }
+
 ipcMain.on('put-in-tray', (event) => {
 	//初始化appIcon
 	const iconPath = path.join(__dirname, 'logo.jpg')
@@ -21,18 +23,8 @@ ipcMain.on('put-in-tray', (event) => {
 	appIcon.setToolTip('软银投资的黄金特色理财平台')
 	appIcon.setTitle('查询中...')
 	appIcon.setContextMenu(contextMenu)
+	updateGoldPrice();
 	//更新appIcon的title
-	setInterval(() => {
-		axios.post('https://www.g-banker.com/price/query', {
-			"queryFlag": 3
-		}).then((response) => {
-			let price = response['data']['data']['realtime_price']
-			price = (price / 100).toFixed(2).toString();
-			appIcon.setTitle(price)
-		}).catch((error) => {
-			console.log(error)
-		})
-	}, 3000)
 })
 ipcMain.on('remove-tray', () => {
 	appIcon.destroy()
@@ -58,8 +50,45 @@ function ready() {
 }
 
 function updateGoldPrice() {
+	if(should_query){
+		console.log('发起行情查询请求');
+		axios.post('https://www.g-banker.com/price/query', {
+			"queryFlag": 3
+		}).then((response) => {
+			let price = response['data']['data']['realtime_price']
+			price = (price / 100).toFixed(2).toString();
+			appIcon.setTitle(price)
+		}).catch((error) => {
+			console.log(error)
+		})
+	}
+	setTimeout(function () {
+		should_query=shouldQuery();
+		updateGoldPrice();
+	},3000)
+}
+
+function shouldQuery() {
 	var date=new moment();
 	var dayOfWeek=date.day()
 	var hourOfDay=date.hour()
 	var minuteOfHour=date.minute()
+	var should=true;
+	switch (dayOfWeek){
+		case 0:
+			should=false
+			break;
+		case 1:
+			should= (hourOfDay>=9 && hourOfDay <=23)
+			break;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			should= (hourOfDay>=0 && hourOfDay<2) || (hourOfDay==2 && minuteOfHour<=30) || (hourOfDay>=9 && hourOfDay <=23)
+		case 6:
+			should= (hourOfDay>=0 && hourOfDay<2) || (hourOfDay==2 && minuteOfHour<=30)
+			break;
+	}
+	return should;
 }
